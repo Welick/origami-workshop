e848579ea80beb957323676bc7d5e17bf359d065
Martin <traffik@abv.bg>
2020-07-14 19:05:15 +0300

    feat: uploading ready state 1

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import Link from '../link'
 import styles from './index.module.css'
+import logo from '../../images/white-origami-bird.png'
 
 const Header = () => {
     return (
         <header className={styles.navigation}>
-            <Link href="#" title="Going to 1" />
-            <Link href="#" title="Going to 2" />
-            <Link href="#" title="Going to 3" />
-            <Link href="#" title="Going to 4" />
+            <img className={styles.logo} src={logo}></img>
+            <Link href="#" title="Going to 1" type={"header"} />
+            <Link href="#" title="Going to 2" type={"header"} />
+            <Link href="#" title="Going to 3" type={"header"} />
+            <Link href="#" title="Going to 4" type={"header"} />
         </header>
     )
 }
--- src/components/origami/index.js
+++ src/components/origami/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import styles from './index.module.css'
+import image from '../../images/blue-origami-bird.png'
 
 const Origami = (props) => {
     return (
         <div className={styles.container}>
+            <img className={styles["origami-image"]} src={image} />
             <p className={styles.description}>
                 {props.description}
             </p>
